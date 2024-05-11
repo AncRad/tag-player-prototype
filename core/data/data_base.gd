c1ca68c4069b07e6c78f4abdd74757af5e992fcd
AncRad <ancrad@yandex.ru>
2024-05-11 06:18:01 +0300

    xxx

--- core/data/data_base.gd
+++ core/data/data_base.gd
@@ -89,52 +89,62 @@ func get_tag_or_create(names : Array[StringName], types : Array[StringName] = []
 func to_bytes() -> PackedByteArray:
 	
 	## создаем сырые данные треков
-	var data_track_key := PackedInt32Array()
-	var data_track_file_path := PackedStringArray()
-	var data_track_tags_types : Array = []
-	var data_track_typed_tags : Array = []
-	for arr in [data_track_key, data_track_file_path, data_track_tags_types, data_track_typed_tags]:
-		arr.resize(_tracks_array.size())
+	#var data_track_key := PackedInt32Array()
+	#var data_track_file_path := PackedStringArray()
+	#var data_track_tags_types : Array = []
+	#var data_track_typed_tags : Array = []
+	#for arr in [data_track_key, data_track_file_path, data_track_tags_types, data_track_typed_tags]:
+		#arr.resize(_tracks_array.size())
+	#
+	#for track_index in data_track_key.size():
+		#var track := _tracks_array[track_index]
+		#
+		#data_track_key.set(track_index, track.key)
+		#data_track_file_path.set(track_index, track.file_path)
+		#
+		#data_track_tags_types[track_index] = [] as Array[StringName]
+		#data_track_tags_types[track_index].assign(track.type_to_tags.keys())
+		#data_track_typed_tags[track_index] = [] as Array[PackedInt32Array]
+		#
+		#for type in data_track_tags_types[track_index]:
+			#var track_tyepd_tags := track.type_to_tags[type] as Array[Tag]
+			#var typed_tags := PackedInt32Array()
+			#typed_tags.resize(track_tyepd_tags.size())
+			#for tag_index in typed_tags.size():
+				#typed_tags.set(tag_index, track_tyepd_tags[tag_index].key)
+			#data_track_typed_tags[track_index].append(typed_tags)
+	#
+	### создаем сырые данные тегов
+	#var data_tag_key := PackedInt32Array()
+	#var data_tag_names : Array[PackedStringArray] = []
+	#var data_tag_color := PackedColorArray()
+	#var data_tag_default_types : Array = []
+	#for arr in [data_tag_key, data_tag_names, data_tag_color, data_tag_default_types]:
+		#arr.resize(_tags_array.size())
+	#
+	#for tag_index in data_tag_key.size():
+		#var tag := _tags_array[tag_index]
+		#
+		#data_tag_key.set(tag_index, tag.key)
+		#data_tag_names[tag_index] = tag.names
+		#data_tag_color.set(tag_index, tag.color)
+		#data_tag_default_types[tag_index] = tag.default_types
+	#
+	#
+	#var bytes : PackedByteArray = var_to_bytes([
+			#data_track_key, data_track_file_path, data_track_tags_types, data_track_typed_tags,
+			#data_tag_key, data_tag_names, data_tag_color, data_tag_default_types,
+	#])
 	
-	for track_index in data_track_key.size():
-		var track := _tracks_array[track_index]
-		
-		data_track_key.set(track_index, track.key)
-		data_track_file_path.set(track_index, track.file_path)
-		
-		data_track_tags_types[track_index] = [] as Array[StringName]
-		data_track_tags_types[track_index].assign(track.type_to_tags.keys())
-		data_track_typed_tags[track_index] = [] as Array[PackedInt32Array]
-		
-		for type in data_track_tags_types[track_index]:
-			var track_tyepd_tags := track.type_to_tags[type] as Array[Tag]
-			var typed_tags := PackedInt32Array()
-			typed_tags.resize(track_tyepd_tags.size())
-			for tag_index in typed_tags.size():
-				typed_tags.set(tag_index, track_tyepd_tags[tag_index].key)
-			data_track_typed_tags[track_index].append(typed_tags)
+	var tags_bytes : Array[PackedByteArray] = []
+	for tag in get_tags():
+		tags_bytes.append(tag.to_bytes())
 	
-	## создаем сырые данные тегов
-	var data_tag_key := PackedInt32Array()
-	var data_tag_names : Array[PackedStringArray] = []
-	var data_tag_color := PackedColorArray()
-	var data_tag_default_types : Array = []
-	for arr in [data_tag_key, data_tag_names, data_tag_color, data_tag_default_types]:
-		arr.resize(_tags_array.size())
+	var tracks_bytes : Array[PackedByteArray] = []
+	for track in get_tracks():
+		tracks_bytes.append(track.to_bytes())
 	
-	for tag_index in data_tag_key.size():
-		var tag := _tags_array[tag_index]
-		
-		data_tag_key.set(tag_index, tag.key)
-		data_tag_names[tag_index] = tag.names
-		data_tag_color.set(tag_index, tag.color)
-		data_tag_default_types[tag_index] = tag.default_types
-	
-	
-	var bytes : PackedByteArray = var_to_bytes([
-			data_track_key, data_track_file_path, data_track_tags_types, data_track_typed_tags,
-			data_tag_key, data_tag_names, data_tag_color, data_tag_default_types,
-	])
+	var bytes := var_to_bytes([tags_bytes, tracks_bytes])
 	var ctx := HashingContext.new()
 	ctx.start(HashingContext.HASH_SHA256)
 	ctx.update(bytes)
@@ -158,34 +168,54 @@ func from_bytes(bytes : PackedByteArray) -> void:
 		assert(not bytes)
 		return
 	
-	var data_track_key := data[0] as PackedInt32Array
-	var data_track_file_path := data[1] as PackedStringArray
-	var data_track_tags_types := data[2] as Array
-	var data_track_typed_tags := data[3] as Array
+	#var data_track_key := data[0] as PackedInt32Array
+	#var data_track_file_path := data[1] as PackedStringArray
+	#var data_track_tags_types := data[2] as Array
+	#var data_track_typed_tags := data[3] as Array
+	#
+	#var data_tag_key := data[4] as PackedInt32Array
+	#var data_tag_names := data[5] as Array
+	#var data_tag_color := data[6] as PackedColorArray
+	#var data_tag_default_types := data[7] as Array
+	#
+	#for tag_index in data_tag_key.size():
+		#var default_types : Array[StringName] = []
+		#default_types.assign(data_tag_default_types[tag_index])
+		#_tag_create(data_tag_names[tag_index], data_tag_color[tag_index], default_types, data_tag_key[tag_index])
+	#
+	#for track_index in data_track_key.size():
+		#var track := _track_create(data_track_file_path[track_index], data_track_key[track_index])
+		#var track_tags_types : Array = data_track_tags_types[track_index]
+		#var track_typed_tags : Array = data_track_typed_tags[track_index]
+		#for tag_type_index in track_tags_types.size():
+			#var tag_type := track_tags_types[tag_type_index] as StringName
+			#var typed_tags : Array[Tag] = []
+			#for tag_key : int in track_typed_tags[tag_type_index]:
+				#var tag : Dictionary = _key_to_tag[tag_key]
+				#typed_tags.append(tag)
+				#track.tag_key_to_type[tag_key] = tag_type
+				#tag.track_key_to_type[track.key] = tag_type
+			#track.type_to_tags[tag_type] = typed_tags
 	
-	var data_tag_key := data[4] as PackedInt32Array
-	var data_tag_names := data[5] as Array
-	var data_tag_color := data[6] as PackedColorArray
-	var data_tag_default_types := data[7] as Array
 	
-	for tag_index in data_tag_key.size():
-		var default_types : Array[StringName] = []
-		default_types.assign(data_tag_default_types[tag_index])
-		_tag_create(data_tag_names[tag_index], data_tag_color[tag_index], default_types, data_tag_key[tag_index])
+	var tags_bytes := data[0] as Array
+	var tracks_bytes := data[1] as Array
 	
-	for track_index in data_track_key.size():
-		var track := _track_create(data_track_file_path[track_index], data_track_key[track_index])
-		var track_tags_types : Array = data_track_tags_types[track_index]
-		var track_typed_tags : Array = data_track_typed_tags[track_index]
-		for tag_type_index in track_tags_types.size():
-			var tag_type := track_tags_types[tag_type_index] as StringName
-			var typed_tags : Array[Tag] = []
-			for tag_key : int in track_typed_tags[tag_type_index]:
-				var tag : Dictionary = _key_to_tag[tag_key]
-				typed_tags.append(tag)
-				track.tag_key_to_type[tag_key] = tag_type
-				tag.track_key_to_type[track.key] = tag_type
-			track.type_to_tags[tag_type] = typed_tags
+	for tag_bytes : PackedByteArray in tags_bytes:
+		var tag := Tag.from_bytes(tag_bytes)
+		_items_array.append(tag)
+		_key_to_item[tag.key] = tag
+		_tags_array.append(tag)
+		_key_to_tag[tag.key] = tag
+		tag.data_base = weakref(self)
+	
+	for track_bytes : PackedByteArray in tracks_bytes:
+		var track := Track.from_bytes(track_bytes, _key_to_tag)
+		_items_array.append(track)
+		_key_to_item[track.key] = track
+		_tracks_array.append(track)
+		_key_to_track[track.key] = track
+		track.data_base = weakref(self)
 	
 	changes_up()
 	update()
@@ -231,7 +261,7 @@ func _tag_create(names : Array[StringName], color := Color.WHITE,
 		key = randi_range(KEY_MIN, KEY_MAX)
 	
 	## создаем экземпляр
-	var tag := Tag.new(key, names, types)
+	var tag := Tag.new(key, names, types, color)
 	tag.data_base = weakref(self)
 	
 	## добавляем в базу данных
@@ -274,7 +304,7 @@ class Item:
 	func to_bytes() -> PackedByteArray:
 		return PackedByteArray()
 	
-	static func from_bytes(bytes : PackedByteArray) -> Item:
+	static func from_bytes(_bytes : PackedByteArray) -> Item:
 		return
 	
 
@@ -306,10 +336,32 @@ class Track extends Item:
 		tag_to_type.clear()
 	
 	func to_bytes() -> PackedByteArray:
-		return PackedByteArray()
+		var type_to_key_tags : Dictionary = {}
+		for type in type_to_tags:
+			var tags : Array[Tag] = type_to_tags[type]
+			var tags_keys := PackedInt32Array()
+			tags_keys.resize(tags.size())
+			for i in tags_keys.size():
+				tags_keys.set(i, tags[i].key)
+			type_to_key_tags[type] = tags_keys
+		return var_to_bytes([key, file_path, type_to_key_tags])
 	
-	static func from_bytes(bytes : PackedByteArray) -> Track:
-		return
+	static func from_bytes(bytes : PackedByteArray, _key_to_tag : Dictionary = {}) -> Track:
+		var data := bytes_to_var(bytes) as Array
+		
+		var track := Track.new(data[0], data[1])
+		
+		var type_to_key_tags := data[2] as Dictionary
+		for type : StringName in type_to_key_tags:
+			var tags_keys := type_to_key_tags[type] as PackedInt32Array
+			var typed_tags := [] as Array[Tag]
+			for tag_key in tags_keys:
+				var tag := _key_to_tag[tag_key] as Tag
+				typed_tags.append(tag)
+				tag.track_key_to_type[track.key] = type
+				track.tag_to_type[tag] = type
+			track.type_to_tags[type] = typed_tags
+		return track
 	
 	func get_tags() -> Array[Tag]:
 		var tags : Array[Tag] = []
@@ -338,10 +390,11 @@ class Tag extends Item:
 	
 	
 	func _init(p_key : int, p_names : Array[StringName], p_types : Array[StringName] = ['default'],
-			color : Color = Color.WHITE) -> void:
+			p_color : Color = Color.WHITE) -> void:
 		key = p_key
 		names = p_names
 		types = p_types
+		color = p_color
 	
 	func tag(track : Track, type : StringName = &'', position : int = -1) -> void:
 		if not type and types.size():
@@ -400,10 +453,17 @@ class Tag extends Item:
 		track_key_to_type.clear()
 	
 	func to_bytes() -> PackedByteArray:
-		return PackedByteArray()
+		return var_to_bytes([key, names, types, color])
 	
-	static func from_bytes(bytes : PackedByteArray) -> Track:
-		return
+	static func from_bytes(bytes : PackedByteArray) -> Tag:
+		var data := bytes_to_var(bytes) as Array
+		@warning_ignore('shadowed_variable')
+		var names := [] as Array[StringName]
+		names.assign(data[1])
+		@warning_ignore('shadowed_variable')
+		var types := [] as Array[StringName]
+		types.assign(data[2])
+		return Tag.new(data[0], names, types, data[3])
 	
 
 #func find_tags(name : String) -> Array[Tag]:
